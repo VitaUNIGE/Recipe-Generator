7ce5a49f47c6be1db8f300459abc3e0a7399db4d
Vitalia Vedenikova <vitalia.vedenikova@etu.unige.ch>
2019-05-09 23:09:48 +0200

    attempt at extracting multiple urls on one page

--- GeneratorRec/app.R
+++ GeneratorRec/app.R
@@ -1,17 +1,29 @@
-library(shiny)
-library(xml2)
 library(rvest)
-library(ggmap)
+library(tidyverse)  
+library(stringr)   
+library(rebus)     
+library(lubridate)
 
-get_site_recipes = function() {
-  Many_Recipes <- read_html(
-    "https://www.cuisineaz.com/"
-  )
+get_recipes = function(){
+  url <- "https://www.allrecipes.com/recipes/1947/everyday-cooking/quick-and-easy/?page=15"
+  webpage <- read_html(url)
+  
+  urls <- recipes_on_webpage %>% 
+    html_nodes ("div.searchresult a") %>% 
+    html_attr("href")
+  read_html(urls)
+  
 }
 
-recipes <- Many_Recipes %>% 
-  html_nodes(".p10") %>%
-  html_text()
 
 
 
+
+TITLE_Recipes_data_html <- html_nodes(webpage,".fixed-recipe-card__h3")
+
+TITLE_data <- html_text( Recipes_data_html )
+
+
+INSTRUCTIONS_data_html <- html_nodes(webpage, "")
+
+INSTRUCTIONS_data <- html_text(INSTRUCTIONS_data_html)
